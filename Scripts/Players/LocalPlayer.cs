af0f4d48d3e4585856cb6f4ce0bdd6dd26abe0a5
d32151477 <57704510+d32151477@users.noreply.github.com>
2023-09-02 09:21:34 +0900

    Update LocalPlayer.cs
    오프셋 수정

--- Scripts/Players/LocalPlayer.cs
+++ Scripts/Players/LocalPlayer.cs
@@ -10,14 +10,12 @@ namespace ApexSharp
             get 
             {
                 if (m_BasePointer == 0)
-                    return m_BasePointer = Memory.Read<long>(Offset.REGION + Offset.LOCAL_PLAYER_ENV);
+                    return m_BasePointer = Memory.Read<long>(Offset.REGION + Offset.LOCAL_PLAYER);
                 return m_BasePointer;
             }
             set => m_BasePointer = value;
         }
 
-        public bool Zooming => Memory.Read<bool>(BasePointer + Offset.ZOOMING);
-        public bool IsDead => Memory.Read<short>(BasePointer + Offset.LIFE_STATE) > 0;
         public Vector3 PunchWeaponAngles => Memory.Read<Vector3>(BasePointer + Offset.VEC_PUNCH_WEAPON_ANGLE);
         public Vector3 BreathAngles => Memory.Read<Vector3>(BasePointer + Offset.BREATH_ANGLES);
         public float TraversalStartTime => Memory.Read<float>(BasePointer + Offset.TRAVERSAL_START_TIME);
