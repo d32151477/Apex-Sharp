10cfe86f6e6aeeefc92d0d1ef0e4f29d6b6e8441
d32151477 <57704510+d32151477@users.noreply.github.com>
2023-09-03 17:31:53 +0900

    Update Sense.cs

--- Scripts/Features/Sense.cs
+++ Scripts/Features/Sense.cs
@@ -8,27 +8,24 @@ namespace ApexSharp
         public void Update(LocalPlayer player, IEnumerable<EntityPlayer> entityPlayers)
         {
             var playerTeamNum = player.TeamNum;
-            if (LocalPlayer.IsPlaying(playerTeamNum))
+            foreach (var entityPlayer in entityPlayers)
             {
-                foreach (var entityPlayer in entityPlayers)
+                if (entityPlayer.Invalid) continue;
+                if (entityPlayer.IsDead) continue;
+                if (entityPlayer.TeamNum == playerTeamNum) continue;
+
+                entityPlayer.Glow = 1;
+                entityPlayer.GlowThroughWall = 2;
+
+                if (entityPlayer.Visible)
+                    entityPlayer.GlowColor = new Vector3(0.0f, 1.0f, 0.0f);
+                else
                 {
-                    if (entityPlayer.Invalid) continue;
-                    if (entityPlayer.IsDead) continue;
-                    if (entityPlayer.TeamNum == playerTeamNum) continue;
-
-                    entityPlayer.Glow = 1;
-                    entityPlayer.GlowThroughWall = 2;
-
-                    if (entityPlayer.Visible)
-                        entityPlayer.GlowColor = new Vector3(0.0f, 1.0f, 0.0f);
-                    else
-                    {
-                        int shield = entityPlayer.ShieldHealth;
-                        if (shield >= 120) entityPlayer.GlowColor = new Vector3(1.0f, 0.0f, 0.0f);
-                        else if (shield >= 100) entityPlayer.GlowColor = new Vector3(0.5f, 0.0f, 1.0f);
-                        else if (shield >= 75) entityPlayer.GlowColor = new Vector3(0.0f, 0.5f, 1.0f);
-                        else entityPlayer.GlowColor = new Vector3(1.0f, 1.0f, 1.0f);
-                    }
+                    int shield = entityPlayer.ShieldHealth;
+                    if (shield >= 120) entityPlayer.GlowColor = new Vector3(1.0f, 0.0f, 0.0f);
+                    else if (shield >= 100) entityPlayer.GlowColor = new Vector3(0.5f, 0.0f, 1.0f);
+                    else if (shield >= 75) entityPlayer.GlowColor = new Vector3(0.0f, 0.5f, 1.0f);
+                    else entityPlayer.GlowColor = new Vector3(1.0f, 1.0f, 1.0f);
                 }
             }
         }
