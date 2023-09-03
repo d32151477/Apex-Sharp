5fa108e3d9c023f45ac5fa373c6c8422582b984b
d32151477 <57704510+d32151477@users.noreply.github.com>
2023-09-03 17:23:11 +0900

    튕김 문제 해결

--- Scripts/Features/Sense.cs
+++ Scripts/Features/Sense.cs
@@ -8,27 +8,27 @@ namespace ApexSharp
         public void Update(LocalPlayer player, IEnumerable<EntityPlayer> entityPlayers)
         {
             var playerTeamNum = player.TeamNum;
-            foreach (var entityPlayer in entityPlayers)
+            if (LocalPlayer.IsPlaying(playerTeamNum))
             {
-                if (entityPlayer.Invalid) continue;
-                if (entityPlayer.IsPlayer is not true) continue;
-                if (entityPlayer.BasePointer == player.BasePointer) continue;
-
-                if (entityPlayer.IsDead) continue;
-                if (entityPlayer.TeamNum == playerTeamNum) continue;
-
-                entityPlayer.Glow = 1;
-                entityPlayer.GlowThroughWall = 2;
-
-                if (entityPlayer.Visible)
-                    entityPlayer.GlowColor = new Vector3(0.0f, 1.0f, 0.0f);
-                else
+                foreach (var entityPlayer in entityPlayers)
                 {
-                    int shield = entityPlayer.ShieldHealth;
-                    if (shield >= 120) entityPlayer.GlowColor = new Vector3(1.0f, 0.0f, 0.0f);
-                    else if (shield >= 100) entityPlayer.GlowColor = new Vector3(0.5f, 0.0f, 1.0f);
-                    else if (shield >= 75) entityPlayer.GlowColor = new Vector3(0.0f, 0.5f, 1.0f);
-                    else entityPlayer.GlowColor = new Vector3(1.0f, 1.0f, 1.0f);
+                    if (entityPlayer.Invalid) continue;
+                    if (entityPlayer.IsDead) continue;
+                    if (entityPlayer.TeamNum == playerTeamNum) continue;
+
+                    entityPlayer.Glow = 1;
+                    entityPlayer.GlowThroughWall = 2;
+
+                    if (entityPlayer.Visible)
+                        entityPlayer.GlowColor = new Vector3(0.0f, 1.0f, 0.0f);
+                    else
+                    {
+                        int shield = entityPlayer.ShieldHealth;
+                        if (shield >= 120) entityPlayer.GlowColor = new Vector3(1.0f, 0.0f, 0.0f);
+                        else if (shield >= 100) entityPlayer.GlowColor = new Vector3(0.5f, 0.0f, 1.0f);
+                        else if (shield >= 75) entityPlayer.GlowColor = new Vector3(0.0f, 0.5f, 1.0f);
+                        else entityPlayer.GlowColor = new Vector3(1.0f, 1.0f, 1.0f);
+                    }
                 }
             }
         }
